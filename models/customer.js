b31bde0abbe883469e2929a055765fb11ab9bd2f
Marylin <marylin@Marylins-MacBook-Pro.local>
2018-03-26 17:15:31 -0500

    doing tier 2 which woops didnt know existed

---
+++ models/customer.js
@@ -0,0 +1,12 @@
+module.exports = function(sequelize, DataTypes){
+    var Customer = sequelize.define("Customer", {
+        customer_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        }
+    });
+    Customer.associate = function (models){
+        Customer.hasMany(models.Burger);
+    }
+    return Customer;
+}
