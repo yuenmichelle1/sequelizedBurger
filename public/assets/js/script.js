f918d3641107520a89e7f7efc6e11eb3b42c920d
Marylin <marylin@Marylins-MacBook-Pro.local>
2018-04-02 01:05:32 -0500

    pleease

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -21,7 +21,7 @@ $(function() {
       }).then(function() {
         // console.log(`test ${result}`)
         console.log("customer added to burger");
-        // location.reload();
+        location.reload();
         // alert("again");
       });
     });
